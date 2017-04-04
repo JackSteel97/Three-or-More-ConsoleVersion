752a439d705b18d73bf6716bc37dde310e447e55
Jack Steel <Jacksteel@jacksteel.co.uk>
2017-04-04 20:39:48 +0100

    adds HistoryEntry class

---
+++ ThreeorMoreConsole/HistoryEntry.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThreeorMoreConsole {
+    class HistoryEntry {
+        private int turnNumber;
+        private Player activePlayer;
+        private Die[] dice;
+
+        public int TurnNumber {
+            get {
+                return this.turnNumber;
+            }
+        }
+        public Player ActivePlayer {
+            get {
+                return this.activePlayer;
+            }
+        }
+        public Die[] Dice {
+            get {
+                return this.dice;
+            }
+        }
+
+        public HistoryEntry(int turnNumber, Player activePlayer, Die[] dice) {
+            if(activePlayer == null || dice == null || dice.Length == 0) {
+                throw new FormatException("No parameters can be null or zero-length");
+            }
+            this.turnNumber = turnNumber;
+            this.activePlayer = activePlayer;
+            this.dice = dice;
+        }
+
+    }
+}
