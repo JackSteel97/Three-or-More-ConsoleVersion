3da83cd7ddee043fbe1042d884e0b1ffe6cdefdd
Jack Steel <Jacksteel@jacksteel.co.uk>
2017-04-04 20:11:56 +0100

    creates die class

---
+++ ThreeorMoreConsole/Die.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThreeorMoreConsole {
+    class Die {
+        private int value;
+        private bool rolled;
+        private int numberOfFaces;
+
+        public int Value {
+            get {
+                return this.value;
+            }
+        }
+        public bool Rolled {
+            get {
+                return this.rolled;
+            }
+            set {
+                this.rolled = value;
+            }
+        }
+        public int NumberOfFaces {
+            get {
+                return this.numberOfFaces;
+            }
+            set {
+                this.numberOfFaces = value;
+            }
+        }
+
+        public int roll() {
+            Random rnd = new Random();
+            int rolledNum = rnd.Next(1, this.numberOfFaces);
+            this.value = rolledNum;
+            return rolledNum;
+        }
+    }
+}
