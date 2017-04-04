2e8971a319f7a540759050d3b6f8de2727014e9a
Jack Steel <Jacksteel@jacksteel.co.uk>
2017-04-04 20:14:24 +0100

    creates Player class

---
+++ ThreeorMoreConsole/Player.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThreeorMoreConsole {
+    class Player {
+        private string name;
+        private int points;
+
+        public string Name {
+            get {
+                return this.name;
+            }
+            set {
+                this.name = value;
+            }
+            
+        }
+        public int Points {
+            get {
+                return this.points;
+            }
+            set {
+                this.points = value;
+            }
+        }
+    }
+}
