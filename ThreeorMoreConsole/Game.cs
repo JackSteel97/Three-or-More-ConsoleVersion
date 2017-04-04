b0fd85a6b074106eb520b01dd4280170b3d094c7
Jack Steel <Jacksteel@jacksteel.co.uk>
2017-04-04 21:20:51 +0100

    adds Game class

---
+++ ThreeorMoreConsole/Game.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThreeorMoreConsole {
+    class Game {
+        private Die[] dice;
+        private Queue<Player> players;
+        private List<HistoryEntry> history;
+        private bool gameOver;
+        private int turnNumber;
+        private int WINNING_SCORE;
+
+        public Game(int scoreToWin, Player[] players, Die[] dice) {
+            if(scoreToWin <= 0) {
+                throw new Exception("Score needed to win must be a positive integer greater than zero.");
+            }
+            this.WINNING_SCORE = scoreToWin;
+            this.turnNumber = 1;
+            this.gameOver = false;
+
+            history.Add(new HistoryEntry("Game Start"));
+
+            foreach(Player player in players) {
+                this.players.Enqueue(player);
+            }
+
+            this.dice = dice;
+
+        }
+    }
+}
