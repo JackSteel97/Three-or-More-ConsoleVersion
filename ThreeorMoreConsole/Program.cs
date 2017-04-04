c737f4154a9cd38ee89b47a9674dc7532f25a273
Jack Steel <Jacksteel@jacksteel.co.uk>
2017-04-04 22:52:38 +0100

    adds player creation

--- ThreeorMoreConsole/Program.cs
+++ ThreeorMoreConsole/Program.cs
@@ -7,6 +7,17 @@ using System.Threading.Tasks;
 namespace ThreeorMoreConsole {
     class Program {
         static void Main(string[] args) {
+            Console.Write("Enter the number of players: ");
+            int playerNum = Convert.ToInt32(Console.ReadLine());
+            List<Player> players = new List<Player>();
+            for(int i = 0; i < playerNum) {
+                Console.Write("\nEnter name for player 1: ");
+                Player player = new Player(Console.ReadLine());
+                players.Add(player);
+            }
+
+            const int NUMBER_OF_DICE = 5;
+
         }
     }
 }
